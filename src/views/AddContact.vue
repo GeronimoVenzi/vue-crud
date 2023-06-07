65f77231876d86f277500dd2b008ed0cde5dd1c4
GeronimoVenzi <geronimovenzi@live.com.ar>
2023-06-07 13:51:07 -0300

    finish Edit and ViewContact

--- src/views/AddContact.vue
+++ src/views/AddContact.vue
@@ -1,5 +1,49 @@
 <template>
-  <h2>Add Contact</h2>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-dark fw-bold">Add contact</p>
+        <p class="fst-italic">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos autem rem magnam quisquam eius? Accusamus aliquid at, odit assumenda quae exercitationem, iure minus nesciunt labore impedit libero tenetur, ducimus eius.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-4">
+        <form >
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Name">
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Photo URL">
+          </div>
+          <div class="mb-2">
+            <input type="email" class="form-control" placeholder="Email">
+          </div>
+          <div class="mb-2">
+            <input type="number" class="form-control" placeholder="Mobile">
+          </div>
+          <div class="mb-2">
+            <input type="number" class="form-control" placeholder="Company">
+          </div>
+          <div class="mb-2">
+            <input type="text" class="form-control" placeholder="Title">
+          </div>
+          <div class="mb-2">
+            <select class="form-control">
+              <option value="">Select group</option>
+            </select>
+          </div>
+          <div class="mb-2">
+            <input type="submit" class="btn btn-success" value="Create">
+          </div>
+        </form>
+      </div>
+      <div class="col-md-4">
+        <img src="https://cdn-icons-png.flaticon.com/512/219/219986.png" alt="" class="contact-img">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
