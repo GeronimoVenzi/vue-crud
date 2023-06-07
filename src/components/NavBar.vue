e3858c8d38c4971457cf124bb28f2c852775ac23
GeronimoVenzi <geronimovenzi@live.com.ar>
2023-06-07 05:10:53 -0300

    generate a button for a new contact and a form whit the same data. in ContactManager

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-dark bg-success navbar-expand-sm">
+  <nav class="navbar navbar-dark bg-dark navbar-expand-sm">
     <div class="container">
         <router-link to="/" class="navbar-brand">
          <i class="fa fa-mobile-alt"> Vue Contact Manager</i>
--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -1,5 +1,69 @@
 <template>
-  <h2>Contact Manager</h2>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-dark fw-bold">Contact Manager
+            <router-link to="/contacts/add" class="btn btn-success btn-sm">
+             <i class="fa fa-plus-circle"></i> New 
+            </router-link>
+        </p>
+        <p class="fst-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quia minima corrupti totam quisquam reprehenderit ducimus temporibus blanditiis deleniti magnam repellendus assumenda consequatur ab et, reiciendis beatae. Excepturi, provident voluptate?</p>
+        <form>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="row">
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Search name">
+                </div>
+                <div class="col">
+                  <input type="submit" class="btn btn-outline-dark" >
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card my-2 list-group-item list-group-item-secondary shadow-lg">
+          <div class="card-body">
+            <div class="row align-items-center">
+              <div class="col-sm-4">
+                <img src="https://cdn-icons-png.flaticon.com/512/219/219986.png" alt="" class="contact-img">
+              </div>
+              <div class="col-sm-7">
+                <ul class="list-group">
+                  <li class="list-group-item">
+                    Name : <span class="fw-bold">Name</span>
+                  </li>
+                  <li class="list-group-item">
+                    Email : <span class="fw-bold">Email</span>
+                  </li>
+                  <li class="list-group-item">
+                    Mobile : <span class="fw-bold">Mobile</span>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center">
+                <router-link to="/contacts/view/:contactId" class="btn btn-warning my-1">
+                  <i class="fa fa-eye"></i>
+                </router-link>
+                <router-link to="/contacts/edit/:contactId" class="btn btn-primary my-1">
+                  <i class="fa fa-pen"></i>
+                </router-link>
+                <button class="btn btn-danger my-1">
+                  <i class="fa fa-trash"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
